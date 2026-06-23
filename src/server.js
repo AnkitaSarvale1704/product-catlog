855bf44b2d9a1aad7b17487889d6869dfb7cf230
AnkitaSarvale1704 <ankitasarvale1704@gmail.com>
2026-06-24 00:26:46 +0530

    Fixed render port configuration

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+
+
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
